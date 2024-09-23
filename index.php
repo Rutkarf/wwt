e7fccd8fdea05ca33fbd080ef716fd502ce74b3f
Rutkarf <bouazza.romain@mail.com>
2024-09-23 10:54:19 +0200

    InitialStatement

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>W0RLD.war.Tek</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <div id="threejs-container"></div>
+    <script src="assets/js/three.js"></script>
+</body>
+</html>
